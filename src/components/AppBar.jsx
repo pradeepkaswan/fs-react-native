e9532d0f21704fa5afa59bbd1747ba8b3c54e865
pradeepkaswan <pradeepkaswan7@gmail.com>
2023-08-24 21:40:56 +0530

    feat: Set up routing for Repositories and Sign In pages, and applied initial styling
    In this commit:
    - Implemented navigation routing for the Repositories and Sign In pages in the React Native app.
    - Applied initial styling to enhance the visual appearance of the pages.

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,54 @@
+import { View, StyleSheet, ScrollView } from 'react-native';
+import Constants from 'expo-constants';
+import { Link } from 'react-router-native';
+
+import Text from './Text';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: theme.colors.appBarBackground
+  },
+  scrollView: {
+    flexDirection: 'row',
+  },
+  tabTouchable: {
+    flexGrow: 0,
+  },
+  tabContainer: {
+    paddingHorizontal: 15,
+    paddingVertical: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tabText: {
+    color: 'white',
+  },
+});
+
+const AppBarTab = ({ children, ...props }) => {
+  return (
+    <Link style={styles.tabTouchable} {...props}>
+      <View style={styles.tabContainer}>
+        <Text fontWeight="bold" style={styles.tabText}>
+          {children}
+        </Text>
+      </View>
+    </Link>
+  );
+};
+
+const AppBar = () => {
+  return (
+    <View style={styles.container}>
+      <ScrollView style={styles.scrollView} horizontal>
+         <AppBarTab to='/' >Repositories</AppBarTab>
+         <AppBarTab to='/sign-in' >Sign in</AppBarTab>
+      </ScrollView>
+    </View>
+  )
+};
+
+export default AppBar;
